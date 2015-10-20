0b24fa2448cefeb51f84a5f86af4c1c4582e0d0c
Alexcouter <alexcouter@gmail.com>
2015-10-20 14:33:45 +0200

    Much more readable

--- BackPack/src/parser/TextParser.java
+++ BackPack/src/parser/TextParser.java
@@ -12,13 +12,13 @@ import backpack.Objet;
 public class TextParser {
 	FileReader input;
 	ArrayList<Objet> objets;
-	
+
 	public TextParser(File fichier) throws FileNotFoundException{
 		super();
 		this.input = new FileReader(fichier);
 		this.objets = new ArrayList<Objet>();
 	}
-	
+
 	protected ArrayList<Objet> parse() throws IOException{
 		BufferedReader bufRead = new BufferedReader(this.input);
 		String ligne = null;
@@ -27,16 +27,21 @@ public class TextParser {
 		{    
 			if(ligne.startsWith("#")){}
 			else{
-			    String[] array1 = ligne.split(" ");
-			    if(array1[1].equals("")){}
-			    else
-			    	objets.add(new Objet(Integer.parseInt(array1[0]),  Integer.parseInt(array1[1])));
+				String[] array1 = ligne.split(" ");
+				if(array1[1].equals("")){}
+				else
+					objets.add(
+							new Objet(
+									Integer.parseInt(array1[0]),  
+									Integer.parseInt(array1[1])
+									)
+							);
 			}
 
 		}
 		return objets;
-		
+
 	}
-	
-	
+
+
 }
